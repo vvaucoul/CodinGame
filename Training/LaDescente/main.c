d3b201d628eeaaea0eb3cfe5f5c6a03bc3370704
vvaucoul <vvaucoul@student.42.fr>
2022-05-02 12:04:37 +0200

    Update

--- Training/LaDescente/main.c
+++ Training/LaDescente/main.c
@@ -3,26 +3,33 @@
 #include <string.h>
 #include <stdbool.h>
 
+static int *find_highest_height(int *heights)
+{
+    int highest_index = 0;
+    int highest_height = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        if (heights[i] > highest_height)
+        {
+            highest_height = heights[i];
+            highest_index = i;
+        }
+    }
+    return ((int [2]){highest_index, highest_height});
+}
+
 int main()
 {
-
-    // game loop
     while (1)
     {
+        int heights[8] = {0};
         for (int i = 0; i < 8; i++)
         {
-            // represents the height of one mountain.
             int mountain_h;
             scanf("%d", &mountain_h);
-
-            printf("%d\n", mountain_h);
+            heights[i] = mountain_h;
         }
-
-        // Write an action using printf(). DON'T FORGET THE TRAILING \n
-        // To debug: fprintf(stderr, "Debug messages...\n");
-
-        printf("1\n");
+        printf("%d\n", find_highest_height(heights)[0]);
     }
-
-    return 0;
+    return (0);
 }
