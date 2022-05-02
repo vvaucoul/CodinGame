d8d3813aea475c19a796e4151f20d5a04754505b
vvaucoul <vvaucoul@student.42.fr>
2022-05-02 13:29:58 +0200

    Update

--- Training/Ascii-Art/main.c
+++ Training/Ascii-Art/main.c
@@ -11,26 +11,43 @@
 int main()
 {
     int L;
-    scanf("%d", &L);
     int H;
+    scanf("%d", &L);
     scanf("%d", &H);
     fgetc(stdin);
+
     char T[257];
     scanf("%[^\n]", T);
+
     fprintf(stderr, "T = %s\n", T);
     fgetc(stdin);
+
+    char AROWS[5][1025];
     for (int i = 0; i < H; i++)
     {
         char ROW[1025];
         scanf("%[^\n]", ROW);
         fgetc(stdin);
-        fprintf(stderr, "ROW[%d] = %s\n", i, ROW);
+        strcpy(AROWS[i], ROW);
+        AROWS[i][L * 26] = '\0';
+    }
+    for (size_t i = 0; i < 5; i++)
+        fprintf(stderr, "AROWS[%d] = %s\n", i, AROWS[i]);
+    for (size_t i = 0; T[i]; i++)
+    {
+        size_t nLetter = T[i] - 'A';
+        fprintf(stderr, "NLetter = %zu\n", nLetter);
+        size_t start_letter_x = L * nLetter;
+        size_t end_letter_x = start_letter_x + L;
+        size_t start_letter_y = 0;
+        size_t end_letter_y = start_letter_y + H;
+
+        char line[1025];
+        fprintf(stderr, " AROWS + start_letter_x = %s\n", AROWS + start_letter_x);
+        strncpy(line, AROWS + start_letter_x, end_letter_x - start_letter_x);
+        fprintf(stderr, "line = %s\n", line);
+        printf("%s\n", line);
     }
 
-    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
-    // To debug: fprintf(stderr, "Debug messages...\n");
-
-    printf("answer\n");
-
-    return 0;
+    return (0);
 }
